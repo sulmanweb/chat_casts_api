4c539f359dd99b731e3a7bac419e90c1b936bae3
Sulman Baig <sulmanweb@gmail.com>
2018-02-24 10:35:03 +0500

    Adding user model with rspec

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  # Email Validations
+  validates :email, presence: true, uniqueness: true, format: {with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, message: I18n.t('error.format_email')}
+
+  # Username Validations
+  validates :username, presence: true, uniqueness: true, format: {with: /\A([a-z\d_]){1,15}\z/, message: I18n.t('error.format_username')}
+
+  # validates password length and regex
+  validates :password, presence: true, on: :create
+  validates :password, format: {with: /\A(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W]).{8,72}\z/, message: I18n.t('error.format_password')}, if: :password
+end
